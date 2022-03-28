ef337a2ba73126e58a7961f0582e5596b2b80316
Lily Xiong <xionglily0605@gmail.com>
2022-03-27 23:41:35 -0400

    - Q1, works but time too slow.

--- src/Q1.java
+++ src/Q1.java
@@ -12,22 +12,28 @@ public class Q1 {
 		int colLen = jail[0][0].length;
 		// number of vertices on each side of the adjacency matrix
 		int numVertices = levelLen * rowLen * colLen;
+
 		// create 2d array for adjacency matrix
-		int[][] adj = new int[numVertices][numVertices];
+//		int[][] adj = new int[numVertices][numVertices];
+
 		int[] startPosition = new int[3];
 		int[] exitPosition = new int[3];
-		createAdjMatrix(jail, adj, startPosition, exitPosition);
+		findStartExitIndices(jail, startPosition, exitPosition);
+
+//		createAdjMatrix(jail, adj, startPosition, exitPosition);
 		// now we have start, end, adjacency matrix
 		// get 1D representation of index
-		int startIndex = AdjMRow(startPosition[0], startPosition[1], startPosition[2], rowLen, colLen);
-		int exitIndex = AdjMRow(exitPosition[0], exitPosition[1], exitPosition[2], rowLen, colLen);
+//		int startIndex = AdjMRow(startPosition[0], startPosition[1], startPosition[2], rowLen, colLen);
+//		int exitIndex = AdjMRow(exitPosition[0], exitPosition[1], exitPosition[2], rowLen, colLen);
+
 		// use dijkstra
-		int[] distances = dijkstra(startIndex, adj, numVertices);
-		print1DArray(distances);
-		if (distances[exitIndex] == Integer.MAX_VALUE) {
+		int[][][] distances = dijkstra(jail, startPosition, numVertices);
+//		print3DArray(distances);
+		int exitDistance = distances[exitPosition[0]][exitPosition[1]][exitPosition[2]];
+		if (exitDistance == Integer.MAX_VALUE) {
 			return -1;
 		}
-		return distances[exitIndex];
+		return exitDistance;
 	}
 	
 
@@ -64,7 +70,131 @@ public class Q1 {
 
 	}
 
-	public static void createAdjMatrix(String[][][] jail, int[][] adj, int[] startPosition, int[] exitPosition) {
+	public static int[] getEastIndices(String[][][] jail, int level, int row, int col) {
+		int[] arr = {level, row, col+1};
+		return arr;
+	}
+	public static int[] getWestIndices(String[][][] jail, int level, int row, int col) {
+		int[] arr = {level, row, col-1};
+		return arr;
+	}
+	public static int[] getNorthIndices(String[][][] jail, int level, int row, int col) {
+		int[] arr = {level, row-1, col};
+		return arr;
+	}
+	public static int[] getSouthIndices(String[][][] jail, int level, int row, int col) {
+		int[] arr = {level, row+1, col};
+		return arr;
+	}
+	public static int[] getUpIndices(String[][][] jail, int level, int row, int col) {
+		int[] arr = {level-1, row, col};
+		return arr;
+	}
+	public static int[] getDownIndices(String[][][] jail, int level, int row, int col) {
+		int[] arr = {level+1, row, col};
+		return arr;
+	}
+
+	public static int canGoEast(String[][][] jail, int[] currPos) {
+		int levelLength = jail.length;
+		int rowLength = jail[0].length;
+		int colLength = jail[0][0].length;
+
+		int level = currPos[0];
+		int row = currPos[1];
+		int col = currPos[2];
+
+		if (col <= colLength - 2) {
+			String east = jail[level][row][col + 1];
+			// pathEast = 1 if has path, 0 if does not have path
+			return hasPath(east);
+		}
+		return 0;
+	}
+
+	public static int canGoWest(String[][][] jail, int[] currPos) {
+		int levelLength = jail.length;
+		int rowLength = jail[0].length;
+		int colLength = jail[0][0].length;
+
+		int level = currPos[0];
+		int row = currPos[1];
+		int col = currPos[2];
+
+		if (col >= 1) {
+			String west = jail[level][row][col - 1];
+			return hasPath(west);
+		}
+		return 0;
+	}
+
+	public static int canGoNorth(String[][][] jail, int[] currPos) {
+		int levelLength = jail.length;
+		int rowLength = jail[0].length;
+		int colLength = jail[0][0].length;
+
+		int level = currPos[0];
+		int row = currPos[1];
+		int col = currPos[2];
+
+		if (row >= 1) {
+			String north = jail[level][row - 1][col];
+			return hasPath(north);
+		}
+		return 0;
+	}
+
+	public static int canGoSouth(String[][][] jail, int[] currPos) {
+		int levelLength = jail.length;
+		int rowLength = jail[0].length;
+		int colLength = jail[0][0].length;
+
+		int level = currPos[0];
+		int row = currPos[1];
+		int col = currPos[2];
+
+		if (row <= rowLength - 2) {
+			String south = jail[level][row + 1][col];
+			return hasPath(south);
+		}
+		return 0;
+	}
+
+	public static int canGoUp(String[][][] jail, int[] currPos) {
+		int levelLength = jail.length;
+		int rowLength = jail[0].length;
+		int colLength = jail[0][0].length;
+
+		int level = currPos[0];
+		int row = currPos[1];
+		int col = currPos[2];
+
+		if (level >= 1) {
+			String up = jail[level - 1][row][col];
+			return hasPath(up);
+		}
+		return 0;
+	}
+
+	public static int canGoDown(String[][][] jail, int[] currPos) {
+		int levelLength = jail.length;
+		int rowLength = jail[0].length;
+		int colLength = jail[0][0].length;
+
+		int level = currPos[0];
+		int row = currPos[1];
+		int col = currPos[2];
+
+		if (level <= levelLength - 2) {
+			String down = jail[level + 1][row][col];
+			return hasPath(down);
+		}
+		return 0;
+	}
+
+
+
+	public static void createAdjMatrix(String[][][] jail, int[][] adjMatrix, int[] startPosition, int[] exitPosition) {
 		String DOT = ".";
 		String POUND = "#";
 		String START = "S";
@@ -78,7 +208,7 @@ public class Q1 {
 		for(int level = 0; level < levelLength; level++) {
 			for(int row = 0; row < rowLength; row++) {
 				for(int col = 0; col < colLength; col++) {
-//					printMatrix(adj);
+//					printMatrix(adjMatrix);
 //					System.out.println(level + "," + row + "," + col);
 					if (!jail[level][row][col].equals(POUND)) {
 						// check if it is start
@@ -97,65 +227,220 @@ public class Q1 {
 						int adjMatrixRow = AdjMRow(level, row, col, rowLength, colLength);
 						int adjMatrixCol = adjMatrixRow;
 						// cell to itself is 1
-						adj[adjMatrixRow][adjMatrixCol] = 1;
+						adjMatrix[adjMatrixRow][adjMatrixCol] = 1;
 
 						if (col <= colLength - 2) {
 							String east = jail[level][row][col + 1];
 							// pathEast = 1 if has path, 0 if does not have path
 							int pathEast = hasPath(east);
 							adjMatrixCol = adjMatrixRow + 1;
-							adj[adjMatrixRow][adjMatrixCol] = pathEast;
-							adj[adjMatrixCol][adjMatrixRow] = pathEast;
+							adjMatrix[adjMatrixRow][adjMatrixCol] = pathEast;
+							adjMatrix[adjMatrixCol][adjMatrixRow] = pathEast;
 						}
 
 						if (col >= 1) {
 							String west = jail[level][row][col - 1];
 							int pathWest = hasPath(west);
 							adjMatrixCol = adjMatrixRow - 1;
-							adj[adjMatrixRow][adjMatrixCol] = pathWest;
-							adj[adjMatrixCol][adjMatrixRow] = pathWest;
+							adjMatrix[adjMatrixRow][adjMatrixCol] = pathWest;
+							adjMatrix[adjMatrixCol][adjMatrixRow] = pathWest;
 						}
 
 						if (row >= 1) {
 							String north = jail[level][row - 1][col];
 							int pathNorth = hasPath(north);
 							adjMatrixCol = adjMatrixRow - colLength;
-							adj[adjMatrixRow][adjMatrixCol] = pathNorth;
-							adj[adjMatrixCol][adjMatrixRow] = pathNorth;
-
+							adjMatrix[adjMatrixRow][adjMatrixCol] = pathNorth;
+							adjMatrix[adjMatrixCol][adjMatrixRow] = pathNorth;
 						}
 
 						if (row <= rowLength - 2) {
 							String south = jail[level][row + 1][col];
 							int pathSouth = hasPath(south);
 							adjMatrixCol = adjMatrixRow + colLength;
-							adj[adjMatrixRow][adjMatrixCol] = pathSouth;
-							adj[adjMatrixCol][adjMatrixRow] = pathSouth;
-
+							adjMatrix[adjMatrixRow][adjMatrixCol] = pathSouth;
+							adjMatrix[adjMatrixCol][adjMatrixRow] = pathSouth;
 						}
 
 						if (level >= 1) {
 							String up = jail[level - 1][row][col];
 							int pathUp = hasPath(up);
 							adjMatrixCol = adjMatrixRow - (rowLength * colLength);
-							adj[adjMatrixRow][adjMatrixCol] = pathUp;
-							adj[adjMatrixCol][adjMatrixRow] = pathUp;
-
+							adjMatrix[adjMatrixRow][adjMatrixCol] = pathUp;
+							adjMatrix[adjMatrixCol][adjMatrixRow] = pathUp;
 						}
+
 						if (level <= levelLength - 2) {
 							String down = jail[level + 1][row][col];
 							int pathDown = hasPath(down);
 							adjMatrixCol = adjMatrixRow + (rowLength * colLength);
-							adj[adjMatrixRow][adjMatrixCol] = pathDown;
-							adj[adjMatrixCol][adjMatrixRow] = pathDown;
+							adjMatrix[adjMatrixRow][adjMatrixCol] = pathDown;
+							adjMatrix[adjMatrixCol][adjMatrixRow] = pathDown;
 						}
 
 					}
 				}
 			}
 		}
-		printMatrix(adj);
+		printMatrix(adjMatrix);
 
+	}
+
+	public static int[][][] dijkstra(String[][][] jail, int[] startPosition, int numVertices) {
+		int levelLength = jail.length;
+		int rowLength = jail[0].length;
+		int colLength = jail[0][0].length;
+
+		// store distances
+		int[][][] distances = new int[levelLength][rowLength][colLength];
+		boolean[][][] finalized = new boolean[levelLength][rowLength][colLength];
+
+		for(int level = 0; level < levelLength; level++) {
+			for (int row = 0; row < rowLength; row++) {
+				for (int col = 0; col < colLength; col++) {
+					distances[level][row][col] = Integer.MAX_VALUE;
+					finalized[level][row][col] = false;
+				}
+			}
+		}
+		// source to itself is 0
+		distances[startPosition[0]][startPosition[1]][startPosition[2]] = 0;
+
+		for (int v = 0; v < numVertices -1; v++) {
+			// get vertex with smallest distances, mimics priority queue
+			int[] minVertex = findClosestVertex(distances, finalized);
+			// update adjacent vertices
+			finalized[minVertex[0]][minVertex[1]][minVertex[2]] = true;
+
+//			for (int j = 0; j < numVertices; j++) {
+//				if(!finalized[j] && adjMatrix[minVertex][j] == 1 && distances[minVertex] != Integer.MAX_VALUE && distances[minVertex] + 1 < distances[j]){
+//					distances[j] = distances[minVertex] + 1;
+//				}
+//			}
+			if (canGoEast(jail, minVertex) == 1) {
+				int[] neighbourIndices = getEastIndices(jail, minVertex[0], minVertex[1], minVertex[2]);
+				int minVertexDistance = distances[minVertex[0]][minVertex[1]][minVertex[2]];
+				int neighbourVertexDistance = distances[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]];
+
+				if (!finalized[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]]
+						&& minVertexDistance != Integer.MAX_VALUE
+						&& minVertexDistance + 1 < neighbourVertexDistance) {
+
+					distances[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]] = minVertexDistance + 1;
+				}
+			}
+			if (canGoWest(jail, minVertex) == 1) {
+				int[] neighbourIndices = getWestIndices(jail, minVertex[0], minVertex[1], minVertex[2]);
+				int minVertexDistance = distances[minVertex[0]][minVertex[1]][minVertex[2]];
+				int neighbourVertexDistance = distances[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]];
+
+				if (!finalized[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]]
+						&& minVertexDistance != Integer.MAX_VALUE
+						&& minVertexDistance + 1 < neighbourVertexDistance) {
+
+					distances[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]] = minVertexDistance + 1;
+				}
+			}
+			if (canGoNorth(jail, minVertex) == 1) {
+				int[] neighbourIndices = getNorthIndices(jail, minVertex[0], minVertex[1], minVertex[2]);
+				int minVertexDistance = distances[minVertex[0]][minVertex[1]][minVertex[2]];
+				int neighbourVertexDistance = distances[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]];
+
+				if (!finalized[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]]
+						&& minVertexDistance != Integer.MAX_VALUE
+						&& minVertexDistance + 1 < neighbourVertexDistance) {
+
+					distances[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]] = minVertexDistance + 1;
+				}
+			}
+			if (canGoSouth(jail, minVertex) == 1) {
+				int[] neighbourIndices = getSouthIndices(jail, minVertex[0], minVertex[1], minVertex[2]);
+				int minVertexDistance = distances[minVertex[0]][minVertex[1]][minVertex[2]];
+				int neighbourVertexDistance = distances[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]];
+
+				if (!finalized[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]]
+						&& minVertexDistance != Integer.MAX_VALUE
+						&& minVertexDistance + 1 < neighbourVertexDistance) {
+
+					distances[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]] = minVertexDistance + 1;
+				}
+			}
+			if (canGoUp(jail, minVertex) == 1) {
+				int[] neighbourIndices = getUpIndices(jail, minVertex[0], minVertex[1], minVertex[2]);
+				int minVertexDistance = distances[minVertex[0]][minVertex[1]][minVertex[2]];
+				int neighbourVertexDistance = distances[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]];
+
+				if (!finalized[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]]
+						&& minVertexDistance != Integer.MAX_VALUE
+						&& minVertexDistance + 1 < neighbourVertexDistance) {
+
+					distances[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]] = minVertexDistance + 1;
+				}
+			}
+			if (canGoDown(jail, minVertex) == 1) {
+				int[] neighbourIndices = getDownIndices(jail, minVertex[0], minVertex[1], minVertex[2]);
+				int minVertexDistance = distances[minVertex[0]][minVertex[1]][minVertex[2]];
+				int neighbourVertexDistance = distances[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]];
+
+				if (!finalized[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]]
+						&& minVertexDistance != Integer.MAX_VALUE
+						&& minVertexDistance + 1 < neighbourVertexDistance) {
+
+					distances[neighbourIndices[0]][neighbourIndices[1]][neighbourIndices[2]] = minVertexDistance + 1;
+				}
+			}
+		}
+		return distances;
+	}
+
+	public static int[] findClosestVertex(int[][][] distances, boolean[][][] finalized){
+		int levelLength = distances.length;
+		int rowLength = distances[0].length;
+		int colLength = distances[0][0].length;
+
+		int minDistance = Integer.MAX_VALUE;
+		int[] closestPosition = {-1, -1, -1};
+		for(int level = 0; level < levelLength; level++) {
+			for (int row = 0; row < rowLength; row++) {
+				for (int col = 0; col < colLength; col++) {
+					int dist = distances[level][row][col];
+					if (!finalized[level][row][col] && dist <= minDistance) {
+						minDistance = dist;
+						closestPosition[0] = level;
+						closestPosition[1] = row;
+						closestPosition[2] = col;
+					}
+				}
+			}
+		}
+		return closestPosition;
+	}
+
+	public static void findStartExitIndices(String[][][] jail, int[] startPosition, int[] exitPosition) {
+		String START = "S";
+		String EXIT = "E";
+
+		int levelLength = jail.length;
+		int rowLength = jail[0].length;
+		int colLength = jail[0][0].length;
+
+		for(int level = 0; level < levelLength; level++) {
+			for (int row = 0; row < rowLength; row++) {
+				for (int col = 0; col < colLength; col++) {
+					if(jail[level][row][col].equals(START)) {
+						startPosition[0] = level;
+						startPosition[1] = row;
+						startPosition[2] = col;
+					}
+
+					else if(jail[level][row][col].equals(EXIT)) {
+						exitPosition[0] = level;
+						exitPosition[1] = row;
+						exitPosition[2] = col;
+					}
+				}
+			}
+		}
 	}
 
 	public static int AdjMRow(int level, int row, int col, int rowLen, int colLen) {
@@ -175,51 +460,9 @@ public class Q1 {
 		return 0;
 	}
 
-	public static int[] dijkstra(int startIndex, int[][] adjMatrix, int numVertices) {
-		// store distances
-		int[] distance = new int[numVertices];
-		boolean[] finalized = new boolean[numVertices];
-		for (int i = 0; i < numVertices; i++) {
-			distance[i] = Integer.MAX_VALUE; // initialize distances to infinity
-			finalized[i] = false;
-		}
-		// source to itself is 0
-		distance[startIndex] = 0;
-
-		for (int v = 0; v < numVertices -1; v++) {
-			// get vertex with smallest distance, mimics priority queue
-			int minVertex = findClosestVertex(distance, finalized);
-			// update adjacent vertices
-			finalized[minVertex] = true;
-			for (int j = 0; j < numVertices; j++) {
-				if(!finalized[j] && adjMatrix[minVertex][j] == 1 && distance[minVertex] != Integer.MAX_VALUE && distance[minVertex] + 1 < distance[j]){
-					distance[j] = distance[minVertex] + 1;
-				}
-			}
-
-		}
-		return distance;
-	}
-
-	public static int findClosestVertex(int[] distance, boolean[] finalized){
-		int minDistance = Integer.MAX_VALUE;
-		int closestIndex = -1;
-		// if we have not finalized the vertex and it has the minimum distance, return the index
-		for (int i = 0; i < distance.length; i++){
-			if(!finalized[i] && distance[i] <= minDistance){
-				minDistance = distance[i];
-				closestIndex = i;
-			}
-		}
-		return closestIndex;
-
-	}
-
-
 	public static void printMatrix(int[][] adj){
 		// Loop through all rows
 		for (int i = 0; i < adj.length; i++) {
-
 			// Loop through all elements of current row
 			for (int j = 0; j < adj[i].length; j++) {
 				System.out.print(adj[i][j] + " ");
@@ -236,4 +479,15 @@ public class Q1 {
 		System.out.println();
 	}
 
+	public static void print3DArray(int[][][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				for (int k = 0; k < arr[0][0].length; k++) {
+					System.out.print(arr[i][j][k] + " ");
+				}
+				System.out.println();
+			}
+			System.out.println("level " + i);
+		}
+	}
 }
